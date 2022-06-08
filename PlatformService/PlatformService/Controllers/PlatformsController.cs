c110cfa672f5bf54742b485657bbc9e6c5c7b477
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-06-07 23:41:46 -0300

    Controllers and actions done

---
+++ PlatformService/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data.Dtos;
+using PlatformService.Data.Intefaces;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepository _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            var platformItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById([FromRoute] int id)
+        {
+            var platform = _repository.GetPlatformById(id);
+
+            if (platform != null)
+                return Ok(_mapper.Map<PlatformReadDto>(platform));
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            try
+            {
+                _repository.CreatePlatform(platformModel);
+                _repository.SaveChanges();
+
+                var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+                return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
