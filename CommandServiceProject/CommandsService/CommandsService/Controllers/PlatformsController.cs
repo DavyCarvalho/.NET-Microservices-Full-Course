2ad27bdefafb20758319271276c3fe9577d3c75b
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-07-27 22:51:04 -0300

    modelo inicial do Command Service

---
+++ CommandServiceProject/CommandsService/CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controller");
+        }
+    }
+}
